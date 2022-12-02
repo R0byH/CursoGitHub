36b5a16c10046ed6ef60d879809f82110090b2e1
limberthfloresflores <limberth.1992.50@gmail.com>
2022-12-02 19:55:53 -0400

    limberth

---
+++ ejercicio2.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import tkinter
+ventana = Frame(height=250,width=450)
+ventana.pack(padx=5,pady=5)
+
+def sumar():
+   resultado= 0
+   Label(text=resultado,font=("Broadway",15),fg="blue").place(width=50,x=200,y=160)
+ 
+def restar():
+   resultado= 0
+   Label(text=resultado,font=("Broadway",15),fg="blue").place(width=50,x=200,y=160)
+
+def multiplicar():
+   resultado= 0
+   Label(text=resultado,font=("Broadway",15),fg="blue").place(width=50,x=200,y=160)
+
+def dividir():
+   resultado= 0
+   Label(text=resultado,font=("Broadway",15),fg="blue").place(width=50,x=200,y=160)
+
+resultado=0
+numero1 = IntVar()
+numero2 = IntVar()
+titulo = Label(text="Operaciones basicas",font=("Algerian",15)).place(x=150,y=30)
+nu1 = Entry(textvariable=numero1).place(width=130,x=180,y=60)
+nu2 = Entry(textvariable=numero2).place(width=130,x=180,y=90)
+bsumar = Button(text="suma",command=sumar).place(width=80, x=50,y=120)
+botonre = Button(text="resta",command=restar).place(width=80, x=150,y=120)
+botonpor = Button(text="multiplicación",command=multiplicar ).place(width=90, x=250,y=120)
+botondi = Button(text="división",command=dividir).place(width=80, x=350,y=120)
+ventana.mainloop()
